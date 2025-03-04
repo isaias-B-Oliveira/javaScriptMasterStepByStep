43d4e12452eff7236926651ce5b659a362f068b8
isaias-B-Oliveira <joseisaias123bezerra@gmail.com>
2025-03-04 15:30:53 -0300

    remane

---
+++ javaScript/1_Console/1-consoleLog.js
@@ -0,0 +1,14 @@
+// O console.log() é um dos métodos mais usados do objeto console em JavaScript. Ele é utilizado para exibir mensagens no console do navegador ou do terminal (caso esteja rodando JavaScript no Node.js).
+// sintaxe console.log(mensagem);
+
+console.log("Hello, World!");
+
+//  para usar o console.log() no terminal, basta rodar o comando node consoleLog.js
+//  mais vc presizar navergar ate o arquivo que vc quer rodar
+//  para entrar numa pasta vc usa o comando (cd nome da pasta)
+//  para sair de uma pasta vc usa o comando (cd ..)
+//  cd javaScriptMasterStepByStep
+//  cd JavaScript
+//  cd Console
+//  node consoleLog.js
+//  Hello, World!
