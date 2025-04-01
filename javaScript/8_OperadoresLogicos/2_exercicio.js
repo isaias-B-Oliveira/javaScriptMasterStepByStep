757e0e69a601b5f87ac526bd6515c86c0ce908f2
isaias-B-Oliveira <joseisaias123bezerra@gmail.com>
2025-04-01 18:40:20 -0300

    exercicio

---
+++ javaScript/8_OperadoresLogicos/2_exercicio.js
@@ -0,0 +1,32 @@
+// Exercício: Verificação de Acesso
+// Você foi contratado para criar um sistema simples que verifica se um usuário pode acessar um site restrito.
+
+// Regras:
+// (1) O usuário pode acessar o site se estiver logado (logado === true) e tiver pelo menos 18 anos.
+
+// (2) Se o usuário for um admin, ele pode acessar o site independentemente da idade.
+
+// (3) Se o usuário não estiver logado, o acesso deve ser negado.
+
+// Tarefa:
+// Crie três variáveis:
+
+// logado (booleano: true ou false)
+
+// idade (número)
+
+// isAdmin (booleano: true ou false)
+
+// Use operadores lógicos (&&, ||, !) para verificar se o usuário pode acessar o site.
+
+// Exiba uma mensagem no console informando se o usuário tem acesso ou não.
+
+// Exemplo de Entrada (1):
+let islogado = true;
+let thisidade = 20;
+let thisisAdmin = false;
+
+// Exemplo de Entrada (2):
+let logado = false;
+let idade = 25;
+let isAdmin = true;
